7e9ec58f08b6ade39ed5483e730efe5b5fc79519
mdtanvirbd <youaretanvirhossain@gmail.com>
2025-03-14 00:11:10 +0600

    Added project files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import profile from '../../assets/images/profile.png'
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center p-4  border-b-2 max-w-7xl mx-auto'>
+            <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+
+        </header>
+    );
+};
+
+export default Header;
